6759326694ef709dc97b95442602735fa8e60525
Antoan Furlanski <antoan.furlanski@gmail.com>
2024-02-16 21:49:45 +0200

    Exam preparation

---
+++ ASPnetFundamentalsExamPrep1/Homies/Data/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Homies.Data
+{
+    public static class DataConstants
+    {
+        public const int NameMinLength = 5;
+        public const int NameMaxLength = 20;
+
+        public const int DescriptionMinLength = 15;
+        public const int DescriptionMaxLength = 150;
+
+        public const string DateFormat = "yyyy-MM-dd H:mm";
+
+        public const int TypeNameMinLength = 5;
+        public const int TypeNameMaxLength = 15;
+    }
+}
---
+++ ASPnetFundamentalsExamPrep1/Homies/Models/EventFormViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Homies.Data.DataConstants;
+using static Homies.Data.ErrorMessages;
+
+namespace Homies.Models
+{
+    public class EventFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(NameMaxLength, 
+            MinimumLength = NameMinLength, 
+            ErrorMessage = StringLengthErrorMessage)]
+
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(DescriptionMaxLength,
+            MinimumLength = DescriptionMinLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        public string Description { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string Start { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string End { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public int TypeId { get; set; }
+
+
+        public IEnumerable<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
